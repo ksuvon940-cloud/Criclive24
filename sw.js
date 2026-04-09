7b42059a25c4bcd4a431c8aceb1adecf8d99c404
ksuvon940-cloud <ksuvon940@gmail.com>
2026-04-10 00:40:36 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
+const CACHE_NAME = "criclive24-cache-v1";
+
+const urlsToCache = [
+  "/Criclive24/",
+  "/Criclive24/index.html",
+  "/Criclive24/logo.png"
+];
+
 self.addEventListener("install", event => {
-  console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Cache opened");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
